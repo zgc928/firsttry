2a7787d57f43e6b8943e4533a7285b83b9590cb6
zgc928 <XXXXXX.com>
2021-04-01 14:48:19 +0800

    master

--- miniprogram/pages/secondpage/secondpage.js
+++ miniprogram/pages/secondpage/secondpage.js
@@ -3,18 +3,18 @@ Page({
         //判断小程序的API，回调，参数，组件等是否在当前版本可用。
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
         isHide: false,
-        src:'',
-        nickname:''
+        src: '',
+        nickname: ''
     },
-  
-    onLoad: function() {
+
+    onLoad: function () {
         var that = this;
         // 查看是否授权
         wx.getSetting({
-            success: function(res) {
+            success: function (res) {
                 if (res.authSetting['scope.userInfo']) {
                     wx.getUserInfo({
-                        success: function(res) {
+                        success: function (res) {
                             // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
                             // 根据自己的需求有其他操作再补充
                             // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
@@ -46,8 +46,8 @@ Page({
             }
         });
     },
-  
-    bindGetUserInfo: function(e) {
+
+    bindGetUserInfo: function (e) {
         if (e.detail.userInfo) {
             //用户按了允许授权按钮
             var that = this;
@@ -57,8 +57,8 @@ Page({
             //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
             that.setData({
                 isHide: false,
-                src:e.detail.userInfo.avatarUrl,
-                nickname:e.detail.userInfo.nickName
+                src: e.detail.userInfo.avatarUrl,
+                nickname: e.detail.userInfo.nickName
             });
         } else {
             //用户按了拒绝按钮
@@ -67,7 +67,7 @@ Page({
                 content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
                 showCancel: false,
                 confirmText: '返回授权',
-                success: function(res) {
+                success: function (res) {
                     // 用户没有授权成功，不需要改变 isHide 的值
                     if (res.confirm) {
                         console.log('用户点击了“返回授权”');
@@ -76,11 +76,15 @@ Page({
             });
         }
     },
-  
-    go_history:function()
-    {
-      wx.navigateTo({
-        url: 'pages/history/history',
-      })
+
+    go_history: function (e) {
+        wx.navigateTo({
+            url: 'pages/history/history',
+            success: (result) => {
+                console.log(success)
+            },
+            fail: () => { },
+            complete: () => { }
+        });
     }
-  })
+})
