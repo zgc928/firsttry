ac76b194ef4a3af575d82e8dc7f24b1f8adb00b3
zgc928 <XXXXXX.com>
2021-04-01 15:15:13 +0800

    master

--- miniprogram/pages/secondpage/secondpage.js
+++ miniprogram/pages/secondpage/secondpage.js
@@ -15,6 +15,10 @@ Page({
                 if (res.authSetting['scope.userInfo']) {
                     wx.getUserInfo({
                         success: function (res) {
+                            that.setData({
+                                src:res.userInfo.avatarUrl
+                            })
+                           
                             // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
                             // 根据自己的需求有其他操作再补充
                             // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
