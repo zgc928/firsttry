e2e73fe0ecd4ef4da2c07db985e690a37e075078
zgc928 <XXXXXX.com>
2021-04-05 18:15:01 +0800

    master

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,9 +4,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    thing: 1,
   },
 
+
+
+
+  do_input(e) {
+    var thing = this.data.thing;
+    console.log(e);
+    this.setData({
+      thing: Number(thing) + Number(e.detail.value)
+    })
+    console.log(e + thing)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -18,49 +29,49 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
-})  
-  
+})
+
